980f216b37819317ea1ff69310f10fdfbcd62b3e
Lina_Herrera <lina_h64@hotmail.com>
2023-11-21 09:53:18 -0500

    task completed

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # EPAMfinalTest
 
+This project aims to demonstrate the implementation of automated tests for the Spotify login functionality using WebdriverIO and Mocha. The tests cover scenarios such as login with empty credentials, login with incorrect credentials, and successful login with correct credentials.
+
+# TASK
 For this task, use the WebDriver IO, Mocha ("describe it" format), Page Object Model. Launch the Spotify Url "https://open.spotify.com/" and the use case to automate would be Login functionality.
 
 UC -1 - Test Login form with empty credentials: 1.Type any credentials 2. Clear the inputs. 3. Check the error messages: 3.1 Please enter your Spotify username or email address. 3.2 Please enter your password.
@@ -16,5 +19,24 @@ Check that Name is correct.
 
 Provide parallel execution, add logging for tests, and use tagging to parametrize tests suite run. Make sure that all tasks supported by this 3 conditions: 1. UC - 1; 2. UC - 2; 3. UC -3.
 
-NOTE: Please use Node version 18.14.1 and wdio verion 8.20.3
+
+## Project Structure
+
+The project structure is organized into two main files:
+
+1. **LoginPage.js**: Defines the Page Object Model (POM) for the Spotify login page, including element locators and login methods.
+
+2. **loginSpotify.spec.js**: Contains the Mocha test scripts for different login scenarios, utilizing the LoginPage POM.
+
+
+## Installation
+To run the application locally, follow these steps:
+
+Clone the repository: git clone https://github.com/lherrer5/EPAMfinalTest
+Install the dependencies: npm install
+Please use Node version 18.14.1 and wdio verion 8.20.3
 To run the tests use the **npm run wdio** command
+
+## Contact
+If you have any questions or suggestions, feel free to contact me at lina_h64@hotmail.com.
+
--- test/specs/loginSpotify.spec.js
+++ test/specs/loginSpotify.spec.js
@@ -1,26 +1,37 @@
 const { expect } = require('@wdio/globals');
 const LoginPage = require('../pageObjects/login.page');
 
-describe('Test Login form', () => {
-  beforeEach(async () => {
-    await browser.url('https://open.spotify.com/');
-  });
-  
-    // it('should display error messages with empty credentials', async () => {
-    //   await LoginPage.login('', '');
-    //   expect(LoginPage.errorMessage.getText()).toBe('Incorrect username or password');
-    // });
+describe('Test Login', () => {
+    beforeEach(async () => {
+        await browser.url('https://open.spotify.com/');
+    });
 
-    // it('should display error message with incorrect credentials', async () => {
-    //     await LoginPage.login('incorrectusername', 'incorrectpassword');
-    //     expect(LoginPage.errorMessage.getText()).toBe('Incorrect username or password.');
-    // });
+    describe('Login with Empty Credentials', () => {
+        it('should display error message when login with empty credentials', async () => {
+            await LoginPage.btnLoginFirst.click();
+            await LoginPage.btnLogin.click();
 
-    it('should successfully log in with correct credentials', async () => {
-        await LoginPage.login('lina_h64@hotmail.com', 'Epamtest123');
+            const errorMessage = await $('span.Message-sc-15vkh7g-0').getText();
+            expect(errorMessage).toContain('Nombre de usuario o contraseña incorrectos');
+        });
+    });
 
-        const profile = await $('figure[data-testid="user-widget-avatar"]');
-        const profileName = await profile.getAttribute('title');
-        expect(profileName).toBe('Lina H');
+    describe('Login with Incorrect Credentials', () => {
+        it('should display error message when login with incorrect credentials', async () => {
+            await LoginPage.login('linah', '23');
+
+            const errorMessage = await $('span.Message-sc-15vkh7g-0').getText();
+            expect(errorMessage).toContain('Nombre de usuario o contraseña incorrectos');
+        });
+    });
+
+    describe('Login with Correct Credentials', () => {
+        it('should successfully log in with correct credentials', async () => {
+            await LoginPage.login('lina_h64@hotmail.com', 'Epamtest123');
+
+            const profile = await $('figure[data-testid="user-widget-avatar"]');
+            const profileName = await profile.getAttribute('title');
+            expect(profileName).toBe('Lina H');
+        });
     });
 });
